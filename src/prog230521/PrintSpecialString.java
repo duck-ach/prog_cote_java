8c2834a4332bf40659a9ee978a5d862f685580b3
duck_ach <djagmlfk@naver.com>
2023-05-21 21:29:36 +0900

    day1

---
+++ src/prog230521/PrintSpecialString.java
@@ -0,0 +1,7 @@
+package prog230521;
+
+public class PrintSpecialString {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
---
+++ src/prog230521/SpringPrint.java
@@ -0,0 +1,12 @@
+package prog230521;
+
+import java.util.Scanner;
+// 문자열 출력하기
+public class SpringPrint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
+
