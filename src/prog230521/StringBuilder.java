736be2bce06abda0ec03a69bd9e8508d5c6ce91c
duck_ach <djagmlfk@naver.com>
2023-05-22 23:43:50 +0900

    day2

--- src/prog230521/StringBuilder.java
+++ src/prog230521/StringBuilder.java
@@ -14,6 +14,8 @@ public class StringBuilder {
         }
 
         System.out.println(result);
+        
+
     }
 
 }
---
+++ src/prog230522/PlusAAndB.java
@@ -0,0 +1,13 @@
+package prog230522;
+
+import java.util.Scanner;
+
+public class PlusAAndB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+}
