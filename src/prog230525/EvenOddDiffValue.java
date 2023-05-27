d28c46ba782baf0dced3cb1fda83ef26a6343357
duck_ach <djagmlfk@naver.com>
2023-05-27 22:21:33 +0900

    condition string

--- src/prog230525/EvenOddDiffValue.java
+++ src/prog230525/EvenOddDiffValue.java
@@ -2,20 +2,11 @@ package prog230525;
 
 public class EvenOddDiffValue {
     public int solution(int n) {
+
         int answer = 0;
 
-        if(n % 2 == 0) {
-            answer = 1;
-            for(int i = 1; i < n; i++) {
-                int ans = (int)Math.pow(i, n);
-                answer += ans;
-            }
-        } else {
-            for(int i = 0; i < n; i++) {
-                answer += i;
-            }
-        }
-
+        for(int i = n; i >= 0; i -= 2)
+            answer += (n % 2 == 0) ? i * i : i;
 
         return answer;
     }
